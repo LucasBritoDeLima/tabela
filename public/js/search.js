7838e9a38899c632714193d2280e19b2f9c127eb
= <lucasbritocientista@gmail.com>
2022-04-07 21:35:46 -0300

    Trazendo os modelos baseado no nome da montadora

--- public/js/search.js
+++ public/js/search.js
@@ -1,19 +1,39 @@
-$(document).ready(() => {
-  $("#form-cars").click((e) => e.preventDefault())
-  var input = $("#brandName").val();
-  let uri = "/car?name=" + input;
-  $("#sendButton").click(() => {
-    console.log(input);
-    $.getJSON(uri, (data) => {
-      let brands = [];
-
-      $(data).each((key,value) => {
-        console.log(value);
-      });
+const formElement = document.querySelector('form');
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log($("#brandName").val());
+  let uri = "/car?name=" + $("#brandName").val();
+  $.getJSON(uri, data => {
+    let brands = [];
+    $("#table-cars").empty();
+    $(data).each((key,value) => {
+      $("#table-cars").append(`<tr>
+      <td class="title-car">${value.name_car}</td>
+      <td class="btn-align">
+        <button class="btn-edit">Editar</button>
+        <button class="btn-del">Apagar</button>
+      </td>
+      </tr>`);
     });
   });
-}
-);
+});
+
+// $(document).ready(() => {
+//   $("#form-cars").click((e) => e.preventDefault())
+//   var input = $("#brandName").val();
+//   let uri = "/car?name=" + input;
+//   $("#sendButton").click(() => {
+//     console.log(input);
+//     $.getJSON(uri, (data) => {
+//       let brands = [];
+
+//       $(data).each((key,value) => {
+//         console.log(value);
+//       });
+//     });
+//   });
+// }
+// );
 
 $(() => {
   $.getJSON("/brand", (data) => {
