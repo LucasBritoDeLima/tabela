ee9d35f29ff17d259f9b65cfd5e0fbb21b85481a
= <lucasbritocientista@gmail.com>
2022-04-12 23:06:48 -0300

    Janela modal

--- public/js/search.js
+++ public/js/search.js
@@ -1,42 +1,42 @@
-const formElement = document.querySelector('form');
-formElement.addEventListener('submit', event => {
+function openModal(a) {
+  $(".modal-container").fadeIn();
+  $(this).css({ "z-index": 99 });
+  $(".overlay").fadeIn(1000);
+  console.log(a);
+}
+
+function closeModal() {
+  $(".modal-container").fadeOut();
+  $(".overlay").fadeOut(1000);
+  $(this).css({ "z-index": 1 });
+}
+
+const formElement = document.querySelector("form");
+formElement.addEventListener("submit", (event) => {
   event.preventDefault();
   let uri = "/car?name=" + $("#brandName").val();
-  $.getJSON(uri, data => {
+  $.getJSON(uri, (data) => {
     let brands = [];
     $("#table-cars").empty();
-    console.log(typeof(data));
-    $(data).each((key,value) => {
-        $("#table-cars").append(`<tr>
+    console.log(typeof data);
+    $(data).each((key, value) => {
+      $("#table-cars").append(`<tr>
       <td class="title-car">${value.name_car}</td>
       <td class="btn-align">
-        <button class="btn-edit">Editar</button>
+        <button class="btn-edit" onclick="openModal(${value.id})">Editar</button>
         <button class="btn-del">Apagar</button>
       </td>
       </tr>`);
     });
   }).fail(() => {
     $("#table-cars").empty();
-    $("#table-cars").append(`<h1 style='color: red'>Não existem dados para essa montadora!</h1>`)
+    $("#table-cars").append(
+      `<h1 style='color: red'>Não existem dados para essa montadora!</h1>`
+    );
   });
 });
 
-// $(document).ready(() => {
-//   $("#form-cars").click((e) => e.preventDefault())
-//   var input = $("#brandName").val();
-//   let uri = "/car?name=" + input;
-//   $("#sendButton").click(() => {
-//     console.log(input);
-//     $.getJSON(uri, (data) => {
-//       let brands = [];
-
-//       $(data).each((key,value) => {
-//         console.log(value);
-//       });
-//     });
-//   });
-// }
-// );
+$(".btn-edit").on("click", () => {});
 
 $(() => {
   $.getJSON("/brand", (data) => {
@@ -48,7 +48,7 @@ $(() => {
     });
 
     $("#brandName").autocomplete({
-      source: items
+      source: items,
     });
   });
 });
